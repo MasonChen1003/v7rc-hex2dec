e6c141c0d235ad49dfc72c73362b2b3a0057f7ff
Shiun Robot DIY <masonchen1003@gmail.com>
2021-07-07 14:53:54 +0800

    Add files via upload

---
+++ README.md
@@ -0,0 +1,34 @@
+![image](Shiun_robot_icon.png)
+   
+# 十六進制轉十進制積木
+輸入為十六進制字串，轉成十進制輸出
+
+主要用於 V7RC 專家模式上解碼使用
+代表值  1000  ->  收到值 HEX : 64
+代表值  1010  ->  收到值 HEX : 65   
+代表值  1500  ->  收到值 HEX : 96 
+代表值  2000  ->  收到值 HEX : C8 
+
+## Code Example 
+==============================================================================
+MiniPOW.set_offset(5, 6, -9)
+
+MiniPOW.motion(MiniPOW.dnum.Stop)
+
+basic.forever(function () {
+	
+})
+
+
+## License
+
+Non-Commercial license.
+
+Author : Mason Chen
+https://www.facebook.com/mason.chen.1420
+
+## Supported targets
+
+* for PXT/microbit
+(The metadata above is needed for package search.)
+
---
+++ main.ts
@@ -0,0 +1,23 @@
+// HEX2DEC function 
+// FB link : https://www.facebook.com/mason.chen.1420
+//% weight=12 color=#ff8533 icon="\uf2d6" block="Shiun robot"
+namespace HEX2DEC {
+        
+    let hex_array = [0123456789abcdef]
+    let dec_out = 0   
+        
+    //% blockId="set_offset" block="convert HEX %hex_in | to DEC"
+    //% weight=90
+    export function hex2dec_func(hex_in: string): void {
+       dec_out = 0
+        for (let bit = 0; bit <= hex_in.length - 1; bit++) {
+            let char = hex_in.charAt(hex_in.length - 1 - bit)
+            for (let ii = 0; ii <= 15; ii++) {
+                if (char.compare(hex_array.charAt(ii)) == 0) {
+                    dec_out = dec_out + ii * Math.pow(16, bit)
+                }
+            }
+        }
+        return dec_out
+    }
+ }
