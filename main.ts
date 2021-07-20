3b879647a81215b0f7a5c19afdc284b8b1e9c8a8
Shiun Robot DIY <masonchen1003@gmail.com>
2021-07-20 09:30:29 +0800

    Update main.ts

--- main.ts
+++ main.ts
@@ -4,6 +4,7 @@
 namespace HEX2DEC {
         
     let hex_array = "0123456789ABCDEF"
+    let hex_array_2 = "0123456789abcdef"
     let dec_out = 0   
         
     //% blockId="hex2dec_func" block="convert HEX %hex_in | to DEC"
@@ -13,7 +14,7 @@ namespace HEX2DEC {
         for (let bit = 0; bit <= hex_in.length - 1; bit++) {
             let char = hex_in.charAt(hex_in.length - 1 - bit)
               for (let ii = 0; ii < 16; ii++) {
-                   if (char.compare(hex_array.charAt(ii)) == 0) {
+                   if ((char.compare(hex_array.charAt(ii)) == 0) || (char.compare(hex_array_2.charAt(ii)) == 0)) {
                       dec_out = dec_out + ii * Math.pow(16, bit)
                    }
               }
